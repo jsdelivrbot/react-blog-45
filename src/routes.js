eb8d053b092995a696101849ae4d984d73aba320
OoOO <oooo@OoOOs-iMac.local>
2017-10-23 14:49:20 -0700

    started blog frontend

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import App from './components/app'
+
+export default (
+    <Route exact path='/' component = {App} />
+)
